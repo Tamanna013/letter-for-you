27bf4ee0ec7390ef4d6756ef81bcf35eca4f8d76
TashxMG <149794391+Tamanna013@users.noreply.github.com>
2025-01-29 21:20:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const envelope = document.querySelector(".love-envelope");
+    envelope.addEventListener("click", function() {
+        this.classList.toggle("open");
+    });
+});
